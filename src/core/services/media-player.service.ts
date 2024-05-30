ff7d4d857461eb7c3c0f59641b73871835c1614b
eurico77 <euricomagalhaes61@gmail.com>
2024-05-30 17:25:18 -0300

    chore(streaming): create services

---
+++ src/core/services/media-player.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import VideoNotFoundException from '../exceptions/video-not-found.exception';
+
+@Injectable()
+export class MediaPlayerService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async prepareStreaming(videoId: string) {
+    const video = await this.prismaService.video.findUnique({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video)
+      throw new VideoNotFoundException(`Video with ${videoId} Not Found`);
+    return video?.url;
+  }
+}
