e7ff0259debb09b28929b44c0c104471a8776b51
eurico77 <euricomagalhaes61@gmail.com>
2024-06-02 11:11:21 -0300

    chore: register modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './persistence/prisma/prisma.module';
 import { PrismaService } from './persistence/prisma/prisma.service';
+import { ContentController } from './http/rest/controllers/content.controller';
+import { MediaPlayerController } from './http/rest/controllers/media-player.controller';
+import { ContentManagementService } from './core/services/content-management.service';
+import { MediaPlayerService } from './core/services/media-player.service';
+import { ContentRepository } from './persistence/repositories/content.repository';
+import { VideoRepository } from './persistence/repositories/video.repository';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [ContentController, MediaPlayerController],
+  providers: [
+    PrismaService,
+    ContentManagementService,
+    MediaPlayerService,
+    ContentRepository,
+    VideoRepository,
+  ],
 })
 export class AppModule {}
